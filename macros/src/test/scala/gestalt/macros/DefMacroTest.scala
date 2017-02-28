1984b7711715e955affbf1c8ee0a60da6fbe2779
Valdis <valdisxp1@gmail.com>
2017-02-28 16:00:10 +0100

    test different prefixes

--- macros/src/test/scala/gestalt/macros/DefMacroTest.scala
+++ macros/src/test/scala/gestalt/macros/DefMacroTest.scala
@@ -10,6 +10,29 @@ class DefMacroTest extends TestSuite {
     val p = new plus2(3)
     assert(p(5) == 8)
   }
+
+  test("plus2 returned value") {
+    case class Holder(item: plus2)
+    val holder = Holder(new plus2(3))
+    assert(holder.item.apply(5) == 8)
+    object Generator{
+      def generate = new plus2(21)
+    }
+    assert(Generator.generate.apply(3) == 24)
+    assert(Generator.generate.apply(3) == 24)
+  }
+
+  test("plus2 on expression") {
+    val three = 3
+    assert(new plus2(3)(5) == 8)
+    assert(new plus2(three * 1)(5) == 8)
+  }
+
+/*  test("plus2 on option get") {
+    val option = Some(new plus2(3))
+    assert(option.get.apply(5) == 8)
+  }*/
+
   test("implicit plus") {
     import ImplicitsForNumbers._
     assert(3.plus(5) == 8)
