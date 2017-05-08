cb0bb6bc27c2c1a57cabebdf652f58272b02bb61
liu fengyun <liu@fengy.me>
2017-05-08 11:11:43 +0200

    remove obsolete code

--- src/main/scala/gestalt/Toolbox.scala
+++ src/main/scala/gestalt/Toolbox.scala
@@ -3,9 +3,7 @@ package scala.gestalt
 case class Location(fileName: String, line: Int, column: Int)
 
 trait Toolbox extends Trees with Types with Denotations with Symbols with TypeTags {
-  val denotations = this
   val symbols = this
-  val types = this
 
   /** get the location where the def macro is used */
   def currentLocation: Location
