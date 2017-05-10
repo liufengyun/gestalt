67b24f16a8eab9d267418942d081537a4ddea210
Valdis <valdisxp1@gmail.com>
2017-05-10 11:03:11 +0200

    composition of formats

--- macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
+++ macros/src/test/scala/gestalt/macros/JsonMacroTest.scala
@@ -11,4 +11,24 @@ class JsonMacroTest extends TestSuite {
     assert(format.fromJson(format.toJson(tom)) == Some(tom))
     assert(format.fromJson(JsObject(Nil)) == None)
   }
+
+  test("composition test"){
+    case class Name(first: String, last: String)
+    case class Registration(name: Name, email: String)
+
+    implicit val nameFormat = JsonMacros.format[Name]()
+    val registrationFormat = JsonMacros.format[Registration]()
+    import JsonMacros._
+
+    val registration = Registration(Name("Tom","Sawyer"),"tom.s@mark.twain.book")
+    val expectedJson = JsObject(Seq(
+      "name" -> JsObject(Seq(
+        "first" -> JsString("Tom"),
+        "last" -> JsString("Sawyer"))),
+      "email" -> JsString("tom.s@mark.twain.book")
+      )
+    )
+    assert(registrationFormat.toJson(registration) == expectedJson)
+    assert(registrationFormat.fromJson(registrationFormat.toJson(registration)) == Some(registration))
+  }
 }
