a56d70887db567e1416782d355d86420cf2be43d
liu fengyun <liu@fengy.me>
2017-05-19 12:00:10 +0200

    change Seq to List in all APIs

--- src/main/scala/gestalt/core/Types.scala
+++ src/main/scala/gestalt/core/Types.scala
@@ -27,25 +27,25 @@ trait Types extends MethodTypes { this: Toolbox =>
     def classSymbol(tp: Type): Option[Symbol]
 
     /** fields of a case class type -- only the ones declared in primary constructor */
-    def caseFields(tp: Type): Seq[Denotation]
+    def caseFields(tp: Type): List[Denotation]
 
     /** field with the given name directly declared in the class */
     def fieldIn(tp: Type, name: String): Option[Denotation]
 
     /** fields directly declared in the class */
-    def fieldsIn(tp: Type): Seq[Denotation]
+    def fieldsIn(tp: Type): List[Denotation]
 
     /** get non-private named methods defined directly inside the class */
-    def methodIn(tp: Type, name: String): Seq[Denotation]
+    def methodIn(tp: Type, name: String): List[Denotation]
 
     /** get all non-private methods defined directly inside the class, exluding constructors */
-    def methodsIn(tp: Type): Seq[Denotation]
+    def methodsIn(tp: Type): List[Denotation]
 
     /** get named non-private methods declared or inherited */
-    def method(tp: Type, name: String): Seq[Denotation]
+    def method(tp: Type, name: String): List[Denotation]
 
     /** get all non-private methods declared or inherited */
-    def methods(tp: Type): Seq[Denotation]
+    def methods(tp: Type): List[Denotation]
 
     /** If `tp` points to a class, the module class of its companion object.
      *  If `tp` points to an object, its companion class.
@@ -59,7 +59,7 @@ trait Types extends MethodTypes { this: Toolbox =>
     def denot(tp: Type): Option[Denotation]
 
     /** The type representing  T[U1, ..., Un] */
-    def appliedTo(tp: Type, args: Seq[Type]): Type
+    def appliedTo(tp: Type, args: List[Type]): Type
 
     /** Turn a type into a typed tree */
     def toTree(tp: Type): tpd.Tree
@@ -79,8 +79,8 @@ trait MethodTypes { this: Types =>
 
   def MethodType: MethodTypeImpl
   trait MethodTypeImpl {
-    def paramInfos(tp: MethodType): Seq[Type]
-    def instantiate(tp: MethodType)(params: Seq[Type]): Type
+    def paramInfos(tp: MethodType): List[Type]
+    def instantiate(tp: MethodType)(params: List[Type]): Type
     def unapply(tp: Type): Option[MethodType]
   }
 }
